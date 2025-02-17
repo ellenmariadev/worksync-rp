c4f2e4f096428e2aaa6110fe95b35626dce9a014
clnhaa <claraschuler6@gmail.com>
2025-02-16 22:05:48 -0300

    chore: create spring boot project

---
+++ backend/src/main/java/com/example/worksync/WorksyncApplication.java
@@ -0,0 +1,13 @@
+package com.example.worksync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorksyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorksyncApplication.class, args);
+	}
+
+}
