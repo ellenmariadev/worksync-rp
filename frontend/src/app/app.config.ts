2d053b42733bc7fa94e9bf0b7ccb5f433b820e6e
Jeffersonalanmm <jefferson.alan.jm@gmail.com>
2025-02-25 14:23:43 -0300

    fix: changing routes file and allowing application to use fetch

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,23 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
-import { HttpInterceptorFn, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 
-export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    const clonedRequest = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${token}`)
-    });
-    return next(clonedRequest);
-  } else {
-    return next(req);
-  }
-};
-
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideHttpClient(withInterceptors([AuthInterceptor])),
-    provideRouter(routes)
-  ]
+  providers: [provideRouter(routes), provideHttpClient(withFetch())],
 };
