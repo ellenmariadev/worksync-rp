6ce312b7871b828bace5a945b1d0ddabb008e236
Jeffersonalanmm <jefferson.alan.jm@gmail.com>
2025-03-04 13:04:41 -0300

    feat: adding not found exception

--- backend/src/main/java/com/example/worksync/exceptions/core/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/worksync/exceptions/core/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.worksync.exceptions.core;
 
 import com.example.worksync.exceptions.ConflictException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+
+import com.example.worksync.exceptions.NotFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -39,5 +42,15 @@ public class GlobalExceptionHandler {
                 e.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e) {
+        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(),
+        HttpStatus.NOT_FOUND.value(),
+        "Not found",
+        e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
 
 }
