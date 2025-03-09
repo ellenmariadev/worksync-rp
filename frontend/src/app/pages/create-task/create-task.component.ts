84310aa18f00f7c0b04955d04a261a7ed73e3ff2
clnhaa <claraschuler6@gmail.com>
2025-03-09 16:46:05 -0300

    feat: edit task

--- frontend/src/app/pages/create-task/create-task.component.ts
+++ frontend/src/app/pages/create-task/create-task.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../../components/navbar/navbar.component'; // Importação do Navbar
 
 @Component({
   selector: 'app-create-task',
-  imports: [NavbarComponent],
+  standalone: true, // Define como um Standalone Component
   templateUrl: './create-task.component.html',
-  styleUrl: './create-task.component.css'
+  styleUrls: ['./create-task.component.css'],
+  imports: [CommonModule, ReactiveFormsModule, NavbarComponent], // Importa os módulos necessários
 })
 export class CreateTaskComponent {
+  taskForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.taskForm = this.fb.group({
+      title: [''],
+      description: [''],
+      project: ['Team projects'],
+      assignedUser: ['User 2'],
+      status: ['Em andamento'],
+    });
+  }
+
+  createTask() {
+    console.log('Tarefa criada:', this.taskForm.value);
+  }
 }
