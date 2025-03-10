0a872cb1bf906bfa8693f8a2d07da57e1f431bee
ellenmariadev <ellenmariadev@gmail.com>
2025-03-10 01:27:21 -0300

    fix(path): organize nested files

--- frontend/src/app/pages/tasks/view-task/view-task.component.ts
+++ frontend/src/app/pages/tasks/view-task/view-task.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NavbarComponent } from '../../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-view-task',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './view-task.component.html',
   styleUrl: './view-task.component.css'
 })
