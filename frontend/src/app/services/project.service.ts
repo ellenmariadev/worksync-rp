a9bb107cb0a71ff5aa16d01da2bf98cd6987943f
Vandielson <vandielson.tenorio.13@hotmail.com>
2025-03-11 01:20:06 -0300

    feat: implementation of project editing and integration with the backend

--- frontend/src/app/services/project.service.ts
+++ frontend/src/app/services/project.service.ts
@@ -21,7 +21,8 @@ export class ProjectsService {
     description: string
   ): Observable<ProjectDTO> {
     const updatedProject = { title, description };
-    return this.http.patch<ProjectDTO>(`${this.apiUrl}/${id}`, updatedProject);
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('token')}`);
+    return this.http.patch<ProjectDTO>(`${this.apiUrl}/${id}`, updatedProject, { headers });
   }
 
   deleteProject(id: number) {
@@ -39,10 +40,25 @@ export class ProjectsService {
     const newProject = { title, description };
     return this.http.post<ProjectDTO>(this.apiUrl, newProject, { headers });
   }
-  
+
   addParticipant(projectId: number, userId: number): Observable<ProjectDTO> {
     const headers = new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('token')}`);
     return this.http.post<ProjectDTO>(`${this.apiUrl}/${projectId}/participants/${userId}`, {}, { headers });
+  }
+
+  getAllUsers(): Observable<any[]> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('token')}`);
+    return this.http.get<any[]>('http://localhost:8080/users', { headers });
+  }
+
+  getProjectById(id: number): Observable<ProjectDTO> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('token')}`);
+    return this.http.get<ProjectDTO>(`${this.apiUrl}/${id}`, { headers });
+  }
+
+  removeParticipant(projectId: number, userId: number): Observable<ProjectDTO> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('token')}`);
+    return this.http.post<ProjectDTO>(`${this.apiUrl}/${projectId}/remove-participant/${userId}`, {}, { headers });
   }
 
 }
