bd748cf46cee57c9a27251cd891dda363e3534a3
ellenmariadev <ellenmariadev@gmail.com>
2025-03-22 14:11:27 -0300

    chore: http method options

--- backend/src/main/java/com/example/worksync/security/SecurityConfig.java
+++ backend/src/main/java/com/example/worksync/security/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.GET, "/projects").permitAll()
