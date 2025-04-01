4ac968f692a97d9ea502865e6b79cba07ce0d8d7
Bianca Maria Cardoso das Neves <bianca.neves@ufape.edu.br>
2025-03-31 22:52:49 -0300

    Update SecurityFilterTest.java

--- backend/src/test/security/SecurityFilterTest.java
+++ backend/src/test/security/SecurityFilterTest.java
@@ -14,7 +14,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.IOException;
 import java.util.Optional;
