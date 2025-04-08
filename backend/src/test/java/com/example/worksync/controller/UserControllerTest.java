30ceb0754c27c5d776b659542b25dec68dc20d26
Bianca Maria Cardoso das Neves <bianca.neves@ufape.edu.br>
2025-04-07 23:28:34 -0300

    Update UserControllerTest.java

--- backend/src/test/java/com/example/worksync/controller/UserControllerTest.java
+++ backend/src/test/java/com/example/worksync/controller/UserControllerTest.java
@@ -5,10 +5,6 @@ import com.example.worksync.service.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,10 +14,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UserController.class)
-class UserControllerTest {
+public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
